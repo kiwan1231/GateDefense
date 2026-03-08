48ba47c731fd8d824308183c90321add086a2fb6
toluciola <tmdrpffhs@naver.com>
2026-03-08 23:47:16 +0900

    Ai 기능 작업중

---
+++ G1/Source/G1/Ai/BTDecorator/BTD_CanAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTD_CanAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class G1_API UBTD_CanAttack : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+};
---
+++ G1/Source/G1/Ai/BTTaskNode/BTT_FindPatrolPos.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTT_FindPatrolPos.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class G1_API UBTT_FindPatrolPos : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
