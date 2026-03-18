819e8cbf6a6cf272a5e487aad8a394f72474df24
toluciola <tmdrpffhs@naver.com>
2026-03-18 17:32:55 +0900

    로딩레벨 및 UI 기능 업데이트 로딩 완료후 게임레벨 실행

---
+++ G1/Source/G1/GameMode/LoadingMode.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameMode/LoadingMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/TimerHandle.h"
+
+ALoadingMode::ALoadingMode(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ALoadingMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TempLoadingTime = TempLoadingMaxTime;
+
+	LoadingDesc = FText::FromString(TEXT("test123.."));
+}
+
+void ALoadingMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	TempLoadingTime = FMath::Clamp(TempLoadingTime - DeltaTime, 0, TempLoadingMaxTime);
+
+	int progress = ((TempLoadingMaxTime - TempLoadingTime) / TempLoadingMaxTime) * 100;
+
+	LoadingDesc = FText::Format(FText::FromString("Loading... {0}%"),progress);
+
+	if (progress == 100 && !bLoadingComplete)
+	{
+		bLoadingComplete = true;
+
+		// 0.5초 뒤 전환
+		GetWorldTimerManager().SetTimer(
+			TimerHandle,
+			this,
+			&ALoadingMode::MoveToNextLevel,
+			0.5f,
+			false
+		);
+	}
+}
+
+void ALoadingMode::MoveToNextLevel()
+{
+	if (bMoveStarted) return;
+
+	bMoveStarted = true;
+
+	UGameplayStatics::OpenLevel(this, NextLevelName);
+}
---
+++ G1/Source/G1/GameMode/LoadingMode.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LoadingMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class G1_API ALoadingMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	ALoadingMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void MoveToNextLevel();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName NextLevelName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText LoadingDesc;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float TempLoadingTime;
+
+private:
+	float TempLoadingMaxTime = 2;
+	bool bLoadingComplete = false;
+	bool bMoveStarted = false;
+	struct FTimerHandle TimerHandle;
+};
